bce72d37503c47044067ca8f44ac3505207feeb2
Giorgio Piatti <contact@giorgiopiatti.ch>
2022-12-24 16:08:29 +0100

    update(main architecture)

--- dilated_gnn_graphgym/loader/prepare_data_loader.py
+++ dilated_gnn_graphgym/loader/prepare_data_loader.py
@@ -1,19 +1,27 @@
+import torch
 from torch_geometric.graphgym.register import register_loader
 from torch_geometric.graphgym.loader import load_pyg, load_ogb
 from torch_geometric.graphgym.config import cfg
 
 from dilated_gnn_graphgym.transform.transform_edge_connectivity import EdgeConnectivity
+from dilated_gnn_graphgym.transform.transform_edge_connectivity_features import EdgeConnectivityAndFeatures
 
-@register_loader('example')
-def load_dataset_example(format, name, dataset_dir):
+
+cfg.gnn.use_edge_features = False
+@register_loader('load_dataset-trasform') #dummy loader, didn't find where to specify which loader
+def load_dataset(format, name, dataset_dir):
+
+    if cfg.gnn.use_edge_features:
+        t = EdgeConnectivityAndFeatures(cfg.gnn.layers_k1, cfg.gnn.layers_k2)
+    else:
+        t = EdgeConnectivity(cfg.gnn.layers_k1, cfg.gnn.layers_k2)
+
     if format == 'PyG':
         dataset = load_pyg(name, dataset_dir)
     elif format == 'OGB':
         dataset = load_ogb(name.replace('_', '-'), dataset_dir)
     else:
         raise ValueError('Unknown data format: {}'.format(cfg.dataset.format))
-
-    t = EdgeConnectivity(cfg.gnn.layers_k1, cfg.gnn.layers_k2)
     dataset.transform = t #set transformer
     return dataset
 
