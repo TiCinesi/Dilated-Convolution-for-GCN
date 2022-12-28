861b04da2eeed9b7a0c5925816b2600300f4f8d0
Giorgio Piatti <contact@giorgiopiatti.ch>
2022-12-28 19:31:45 +0100

    update(agg code)

--- agg_runs.py
+++ agg_runs.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -8,7 +9,6 @@ from torch_geometric.graphgym.utils.io import (
     dict_list_to_json,
     dict_list_to_tb,
     dict_to_json,
-    json_to_dict_list,
     makedirs_rm_exist,
     string_to_python,
 )
@@ -42,19 +42,32 @@ def join_list(l1, l2):
     return l1
 
 
-def agg_dict_list(dict_list):
+def agg_dict_list(dict_list, best=False, epoch_name=None):
     """
     Aggregate a list of dictionaries: mean + std
     Args:
         dict_list: list of dictionaries
 
     """
-    dict_agg = {'epoch': dict_list[0]['epoch']}
+    if best:
+        dict_agg = {'epoch': 'val_best'}
+    else:
+        dict_agg = {'epoch': dict_list[0]['epoch']}
+
+    if epoch_name is not None:
+        dict_agg = {'epoch': epoch_name}
+
     for key in dict_list[0]:
         if key != 'epoch':
             value = np.array([dict[key] for dict in dict_list])
             dict_agg[key] = np.mean(value).round(cfg.round)
             dict_agg['{}_std'.format(key)] = np.std(value).round(cfg.round)
+        else:
+            if epoch_name is not None or best:
+                value = np.array([dict[key] for dict in dict_list])
+                dict_agg['ckpt_epoch'] = np.mean(value).round(cfg.round)
+                dict_agg['{}_std'.format('ckpt_epoch')] = np.std(value).round(cfg.round)
+        
     return dict_agg
 
 
@@ -79,6 +92,22 @@ def rm_keys(dict, keys):
         dict.pop(key, None)
 
 
+def json_to_dict_list(fname, split):
+    dict_list = []
+    #epoch_set = set()
+    with open(fname) as f:
+        lines = f.readlines()
+        if split == 'val': #NOTE skip logs of sanity check dataloader
+            lines = lines[1:]
+
+        for line in lines:
+            line = line.rstrip()
+            dict = json.loads(line)
+            # if dict['epoch'] not in epoch_set:
+            dict_list.append(dict)
+            #epoch_set.add(dict['epoch'])
+    return dict_list
+
 def agg_runs(dir, metric_best='auto'):
     r'''
     Aggregate over different random seeds of a single experiment
@@ -99,7 +128,7 @@ def agg_runs(dir, metric_best='auto'):
             if split in os.listdir(dir_seed):
                 dir_split = os.path.join(dir_seed, split)
                 fname_stats = os.path.join(dir_split, 'stats.json')
-                stats_list = json_to_dict_list(fname_stats)
+                stats_list = json_to_dict_list(fname_stats, split)
                 if metric_best == 'auto':
                     metric = 'auc' if 'auc' in stats_list[0] else 'accuracy'
                 else:
@@ -116,15 +145,12 @@ def agg_runs(dir, metric_best='auto'):
                 if is_split(split):
                     dir_split = os.path.join(dir_seed, split)
                     fname_stats = os.path.join(dir_split, 'stats.json')
-                    stats_list = json_to_dict_list(fname_stats)
+                    stats_list = json_to_dict_list(fname_stats, split)
                     
-                    if split == 'test':
-                        stats_best = stats_list[0]
-                    else:
-                        stats_best = [
-                            stats for stats in stats_list
-                            if stats['epoch'] == best_epoch
-                        ][0]
+                    stats_best = [
+                        stats for stats in stats_list
+                        if stats['epoch'] == best_epoch
+                    ][0]
 
                     print(stats_best)
                     stats_list = [[stats] for stats in stats_list]
@@ -142,9 +168,14 @@ def agg_runs(dir, metric_best='auto'):
                     if v is not None}  # rm None
     for key in results:
         for i in range(len(results[key])):
-            results[key][i] = agg_dict_list(results[key][i])
+            if key == 'test' and i == 0:
+                results[key][i] = agg_dict_list(results[key][i], epoch_name='last_epoch')
+            elif key == 'test' and i == 1:
+                results[key][i] = agg_dict_list(results[key][i], epoch_name='best_epoch')
+            else:
+                results[key][i] = agg_dict_list(results[key][i])
     for key in results_best:
-        results_best[key] = agg_dict_list(results_best[key])
+        results_best[key] = agg_dict_list(results_best[key], best=True)
     # save aggregated results
     for key, value in results.items():
         dir_out = os.path.join(dir, 'agg', key)
@@ -152,13 +183,13 @@ def agg_runs(dir, metric_best='auto'):
         fname = os.path.join(dir_out, 'stats.json')
         dict_list_to_json(value, fname)
 
-        if cfg.tensorboard_agg:
-            if SummaryWriter is None:
-                raise ImportError(
-                    'Tensorboard support requires `tensorboardX`.')
-            writer = SummaryWriter(dir_out)
-            dict_list_to_tb(value, writer)
-            writer.close()
+        # if cfg.tensorboard_agg:
+        #     if SummaryWriter is None:
+        #         raise ImportError(
+        #             'Tensorboard support requires `tensorboardX`.')
+        #     writer = SummaryWriter(dir_out)
+        #     dict_list_to_tb(value, writer)
+        #     writer.close()
     for key, value in results_best.items():
         dir_out = os.path.join(dir, 'agg', key)
         fname = os.path.join(dir_out, 'best.json')
