b0ad0bc09eca38a70a6c38cc2d62bee80c46387a
Giorgio Piatti <contact@giorgiopiatti.ch>
2023-01-02 15:05:15 +0100

    clean

--- dilated_gnn_graphgym/transform/transform_edge_connectivity.py
+++ dilated_gnn_graphgym/transform/transform_edge_connectivity.py
@@ -16,36 +16,54 @@ class EdgeConnectivity(BaseTransform):
         if data.x is None:
             data.x = torch.zeros((data.num_nodes,1), dtype=torch.float)
 
-        #TODO compute feature data (new class)
-        if data.is_directed():
-            raise ValueError("Graph is directed, cannot proceed!")
-        
-        G = to_networkx(data, to_undirected=True, remove_self_loops=True)
-
         distance_graphs = []
-        distance_graphs_attr_ids = []
         for i in range(self.k2):
             distance_graphs.append([[], []])
-            distance_graphs_attr_ids.append([])
-        
-        for v in range(G.number_of_nodes()):
-            depth_limit = int(2*math.pow(3, self.k2-1)*self.k1)
-            bsf_tree = nx.bfs_tree(G, source=v, depth_limit=depth_limit)
-            dist_layes = dict(enumerate(nx.bfs_layers(bsf_tree, [v])))
-
-            for i in range(self.k2):
-                d = int(2*math.pow(3, i)*self.k1)
-                
-                if not d in dist_layes.keys():
-                    #no nodes in d-th level set
-                    continue
-
-                for u in dist_layes[d]:
-                    distance_graphs[i][0].append(u)
-                    distance_graphs[i][1].append(v)
-        
     
+
+        if data.is_directed():
+            G = to_networkx(data, to_undirected=False, remove_self_loops=True)
+
+
+            for v in range(G.number_of_nodes()):
+                depth_limit = int(math.pow(2, self.k2-1)*self.k1)
+                bsf_tree = nx.bfs_tree(nx.reverse(G), source=v, depth_limit=depth_limit)
+                dist_layes = dict(enumerate(nx.bfs_layers(bsf_tree, [v])))
+                bsf_tree = nx.reverse(bsf_tree)
+                
+                
+                for i in range(self.k2):
+                    d = int(math.pow(2, i)*self.k1)
+                            
+                    if not d in dist_layes.keys():
+                        #no nodes in d-th level set
+                        continue
+
+                    for u in dist_layes[d]:
+                        distance_graphs[i][0].append(u)
+                        distance_graphs[i][1].append(v)
+        
+        else:
+            G = to_networkx(data, to_undirected=True, remove_self_loops=True)
+
+            for v in range(G.number_of_nodes()):
+                depth_limit = int(2*math.pow(3, self.k2-1)*self.k1)
+                bsf_tree = nx.bfs_tree(G, source=v, depth_limit=depth_limit)
+                dist_layes = dict(enumerate(nx.bfs_layers(bsf_tree, [v])))
+                
+                for i in range(self.k2):
+                    d = int(2*math.pow(3, i)*self.k1)
+                            
+                    if not d in dist_layes.keys():
+                        #no nodes in d-th level set
+                        continue
+
+                    for u in dist_layes[d]:
+                        distance_graphs[i][0].append(u)
+                        distance_graphs[i][1].append(v)
+        
         for i in range(self.k2):
+
             edges = torch.tensor(distance_graphs[i], dtype=torch.int64)
             if edges.shape[0] == 0: 
                 edges = torch.empty((2,0), dtype=torch.int64)
