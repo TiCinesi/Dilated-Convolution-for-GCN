3ebcbece665ca4eb2257ddac4b99e5032e0470c4
Giorgio Piatti <contact@giorgiopiatti.ch>
2023-01-07 10:40:39 +0100

    refactor(dataset)

--- dilated_gnn_graphgym/loader/bottleneck.py
+++ dilated_gnn_graphgym/loader/bottleneck.py
@@ -160,12 +160,42 @@ from torch_geometric.data.separate import separate
 from torch_geometric.data.collate import collate
 import copy
 from joblib import Parallel, delayed
+from collections.abc import Mapping, Sequence
+from typing import Callable, Dict, Iterable, List, Optional, Tuple, Union
+def nested_iter(node: Union[Mapping, Sequence]) -> Iterable:
+    if isinstance(node, Mapping):
+        for key, value in node.items():
+            for inner_key, inner_value in nested_iter(value):
+                yield inner_key, inner_value
+    elif isinstance(node, Sequence):
+        for i, inner_value in enumerate(node):
+            yield i, inner_value
+    else:
+        yield None, node
+
 
 class BottleneckDataset(Dataset):
+    @staticmethod
+    def collate(
+            data_list: List[Data]) -> Tuple[Data, Optional[Dict[str, Tensor]]]:
+        r"""Collates a Python list of :obj:`torch_geometric.data.Data` objects
+        to the internal storage format of
+        :class:`~torch_geometric.data.InMemoryDataset`."""
+        if len(data_list) == 1:
+            return data_list[0], None
+
+        data, slices, _ = collate(
+            data_list[0].__class__,
+            data_list=data_list,
+            increment=False,
+            add_batch=False,
+        )
+
+        return data, slices
+
     def __init__(self, data_list, transform=None, pre_transform=None, pre_filter=None, split=None):
         super().__init__(None, transform, pre_transform, pre_filter)
         self.split = split
-        self.data_list = data_list
         self.original_len = len(data_list)
         if pre_transform is not None:
             # @ray.remote
@@ -176,20 +206,48 @@ class BottleneckDataset(Dataset):
             # p = ray.put(self.pre_transform)
             # self.data_list = ray.get([f.remote(d,p) for d in data_list])
 
-            res = Parallel(n_jobs=-1)(delayed(self.pre_transform)(d) for d in data_list)
-            self.data_list = res
+            data_list = Parallel(n_jobs=-1)(delayed(self.pre_transform)(d) for d in data_list)
+            print('Done trasform for ', split)
+
+
+        self._data_list: Optional[List[Data]] = None
+        self.data, self.slices = self.collate(data_list)
+
 
     def len(self) -> int:
-        if self.split == 'train':
-            return len(self.data_list)*1000
-        else:
-            return len(self.data_list)
+        if self.slices is None:
+            return 1
+        for _, value in nested_iter(self.slices):
+            if self.split == 'train':
+                return (len(value) - 1)*100
+            else:
+                return len(value) - 1
+        return 0
 
 
     def get(self, idx: int) -> Data:
         if self.split == 'train':
             idx = idx % self.original_len
-        return copy.copy(self.data_list[idx])
+        
+        if self.len() == 1:
+            return copy.copy(self.data)
+
+        if not hasattr(self, '_data_list') or self._data_list is None:
+            self._data_list = self.len() * [None]
+        elif self._data_list[idx] is not None:
+            return copy.copy(self._data_list[idx])
+
+        data = separate(
+            cls=self.data.__class__,
+            batch=self.data,
+            idx=idx,
+            slice_dict=self.slices,
+            decrement=False,
+        )
+
+        self._data_list[idx] = copy.copy(data)
+
+        return data
 
 cfg.share.dim0 = -1
 def create_bottleneck_dataloader(name, dataset_dir, transform, pre_transform):
